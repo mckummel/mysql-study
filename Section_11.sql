2a097a2d1d69464f3179ef4d077c60d870bf8c66
Matheus K <me@matheuskummel.com>
2018-04-29 13:26:32 -0300

    Section 11 case statements

--- Section_11.sql
+++ Section_11.sql
@@ -96,3 +96,40 @@ WHERE released_year >= 2000
 SELECT title, released_year FROM books
 WHERE released_year >= 2000 
 	  AND released_year % 2 != 0;
+      
+-- case statements
+
+SELECT title, released_year , 
+	CASE WHEN released_year >= 2000 THEN 'Modern Literature'
+    ELSE '20th Century Literature'
+	END AS Genre
+FROM books;
+
+SELECT title, released_year , 
+	CASE WHEN released_year >= 2000 THEN 'Modern Literature'
+    ELSE '20th Century Literature'
+	END -- table header becomes huge without AS ...
+FROM books;
+
+SELECT title, stock_quantity , 
+	CASE WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+    WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+    ELSE '***'
+	END AS stock
+FROM books;
+
+SELECT title, stock_quantity , 
+	CASE WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+    WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+    WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+    ELSE '****'
+	END AS stock
+FROM books;
+
+SELECT title, stock_quantity , 
+	CASE WHEN stock_quantity <= 50 THEN '*'
+    WHEN stock_quantity <= 100 THEN '**'
+    WHEN stock_quantity <= 150 THEN '***'
+    ELSE '****'
+	END AS stock
+FROM books;
