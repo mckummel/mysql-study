7907d07cd7c5d4291ee9db6c131ef569fd4fef7a
Matheus K <me@matheuskummel.com>
2018-05-07 17:27:27 -0300

    Section 16 - mysql node.js package

--- app.js
+++ app.js
@@ -10,9 +10,29 @@ for(var i = 0; i < 500; i++)
 */
 
 var faker = require('faker');
+var mysql = require('mysql');
 
-console.log(faker.internet.email());
-generateAddress();
+var connection = mysql.createConnection
+({
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'section16'
+});
+
+var query1 = 'SELECT 1+1 AS solution';
+var query2 = 'SELECT CURDATE()';
+var query3 = 'SELECT 1+5';
+var query4 = 'SELECT 1+5 AS answer';
+var query5 = 'SELECT CURTIME() as time, CURDATE() as date, NOW() as now';
+
+connection.query(query5, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results[0].time);
+  console.log(results[0].date);
+  console.log(results[0].now);
+});
+connection.end();
 
 function generateAddress()
 {
@@ -20,3 +40,5 @@ function generateAddress()
     console.log(faker.address.city());
     console.log(faker.address.streetAddress());
 }
+
+
