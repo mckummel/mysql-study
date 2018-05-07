d6df38ba5133c1466d59523c954c0b2ac25676f0
Matheus K <me@matheuskummel.com>
2018-05-07 16:28:33 -0300

    Section 15 last 3 exercises

--- Section_15.sql
+++ Section_15.sql
@@ -22,5 +22,22 @@ GROUP BY photos.id
 ORDER BY likes_qty DESC
 LIMIT 1;
 
+SELECT 
+	(SELECT COUNT(*) FROM photos) 
+		/ 
+	(SELECT COUNT(*) FROM users);
+    
+SELECT tags.tag_name,COUNT(photo_tags.tag_id) AS tag_qty FROM photo_tags
+INNER JOIN tags ON photo_tags.tag_id = tags.id
+GROUP BY photo_tags.tag_id
+ORDER BY tag_qty DESC
+LIMIT 5;
+
+SELECT users.username,users.id, COUNT(*) AS total_likes FROM users
+INNER JOIN likes ON users.id = likes.user_id
+GROUP BY users.id
+HAVING total_likes = (SELECT COUNT(*) FROM photos);
+-- having is the same as where but we can use it for groups
+
 
 
